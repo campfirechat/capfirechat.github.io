f3b001d74db3f6e698a98d0cfc3e984eb2f1058f
campfirechat <100876681+campfirechat@users.noreply.github.com>
2022-03-04 11:16:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function myFunction(){
     alert("DID YOU JUST PRESS ON ME?");
     alert("HOW DARE YOU");
 }
+function multiplication(){
+    document.getElementById("demo").innerHTML = "Hell yea"
+}
